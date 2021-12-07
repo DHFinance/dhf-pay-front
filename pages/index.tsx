fca5d98f1b2ba797c90e23f834e85580549e8bbf
JX-29 <anton23490@gmail.com>
2021-12-07 12:54:15 +0300

    root page withAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next'
 import SliderContainer from "../src/components/Layout/SliderContainer";
+import WithAuth from "../hoc/withAuth";
 
 const Home: NextPage = () => {
   return (
-    <SliderContainer>Это стартовая страница, контент пока что отсутствует</SliderContainer>
+    <WithAuth><SliderContainer>Это стартовая страница, контент пока что отсутствует</SliderContainer></WithAuth>
   )
 }
 
