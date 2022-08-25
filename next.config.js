6f2c212a3718e0337457582e3ce5ec989440d7df
reppin <andrey.krylov@smartigy.ru>
2022-08-25 12:54:48 +0300

    fix-bill-page

--- next.config.js
+++ next.config.js
@@ -3,8 +3,16 @@ const withCSS = require('@zeit/next-css');
 const path = require('path')
 
 
+const ContentSecurityPolicy = `
+  default-src 'unsafe-inline';
+  script-src 'unsafe-inline';
+  child-src https://dhfi.online;
+  style-src 'unsafe-inline' https://dhfi.online;
+  font-src 'unsafe-inline';
+`
+
 let nextConfig = {
-  presets: ["next/babel"],
+  presets: ['next/babel'],
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
@@ -12,10 +20,23 @@ let nextConfig = {
     return [
       {
         source: '/rpc/:path*',
-        destination: 'http://3.14.161.135:7777/rpc/:path*' // Proxy to Backend
-      }
+        destination: 'http://3.14.161.135:7777/rpc/:path*', // Proxy to Backend
+      },
     ]
-  }
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: [{
+          key: 'Content-Security-Policy',
+          value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+        }
+        ],
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
