e4765db0db5a3c87dcb5b42a844f49a90bb0f722
reppin <andrey.krylov@smartigy.ru>
2022-08-25 14:05:59 +0300

    fix-config

--- next.config.js
+++ next.config.js
@@ -3,13 +3,7 @@ const withCSS = require('@zeit/next-css');
 const path = require('path')
 
 
-const ContentSecurityPolicy = `
-  default-src 'unsafe-inline';
-  script-src 'unsafe-inline';
-  child-src https://dhfi.online;
-  style-src 'unsafe-inline' https://dhfi.online;
-  font-src 'unsafe-inline';
-`
+const ContentSecurityPolicy = `script-src 'unsafe-inline'`;
 
 let nextConfig = {
   presets: ['next/babel'],
@@ -31,7 +25,7 @@ let nextConfig = {
         source: '/:path*',
         headers: [{
           key: 'Content-Security-Policy',
-          value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+          value: ContentSecurityPolicy
         }
         ],
       },
